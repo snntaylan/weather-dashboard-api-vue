cb527eb8a1da8afbd33434c932e9a326ae7f267e
snntaylan <sinan.taylan@outlook.com>
2019-04-09 17:22:41 +0200

    first commit

---
+++ weather-dashboard-api/src/main.js
@@ -0,0 +1,16 @@
+import "reset-css";
+import "es6-promise/auto";
+import "whatwg-fetch";
+import Vue from "vue";
+import App from "./App";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  store,
+  template: "<App/>",
+  components: { App }
+});
